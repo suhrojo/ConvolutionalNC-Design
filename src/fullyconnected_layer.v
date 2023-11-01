8700de400bce011326cb05290a17f7559e7aaabe
suhrojo <rojel1200@gmail.com>
2023-10-31 20:05:25 -0700

    +convLayer, +FCLayer, +PoolingLayer, Created Hidden Layer file

---
+++ src/fullyconnected_layer.v
@@ -0,0 +1,11 @@
+`default_nettype none
+
+module FCL ( // Full Connected Layer
+    input wire [7:0] data_in,
+    input wire clk,
+    input wire rst_n, 
+
+    output reg [7:0] data_out
+);
+
+endmodule
