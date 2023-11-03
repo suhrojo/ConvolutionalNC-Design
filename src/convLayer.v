87062bad1b0cb5ad7c1ab1247bc7e5c640c26d2c
suhrojo <rojel1200@gmail.com>
2023-11-03 13:51:57 -0700

    pushing before trying to set a predefined kernel

--- src/convLayer.v
+++ src/convLayer.v
@@ -1,34 +1,34 @@
 `default_nettype none
 
 module convLayer (
-    input wire clk,
-    input wire rst_n, 
-    input wire [5:0][5:0] data_in,
-    input wire [5:0][5:0] kernel,
+    input wire clk, // clk input
+    input wire rst_n, // reset input
+    input wire [5:0][5:0] data_in, //6x6 data_in from switches
+    input wire [5:0][5:0] kernel, //6x6 filter kernel
 
-    output reg data_out
+    output reg data_out //feature_map output
 );
-    reg feature_map;
+    reg [5:0][5:0] feature_map; //feature_map output before summation of products
+    reg [5:0] sum; //feature_map sum
+    integer i, j;
 
-    // apply convolution: 6x6x1 input * filter 1: kernel = 1x1x1 feature map
-    always @(posedge clk) begin
+    // apply convolution: 6x6x1 input * filter 1: kernel = 6x6x1 feature map
+    always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            data_out <= 0;
-            feature_map <= 0;
+            data_out <= 6'd0; // data_out reset to 0
         end
         else begin
-            int sum = 0;
-            for (int i = 0; i < 6; i++) begin
-                for (int j = 0; j < 6; j++) begin
-                    sum = sum + (data_in[i][j] * kernel[i][j]);
+            sum <= 6'd0; // reset sum for holding all 36
+            for (i = 0; i < 6; i = i + 1) begin
+                for (j = 0; j < 6; j = j + 1) begin
+                    feature_map[i][j] <= data_in[i][j] * kernel[i][j]; //applying kernel filter
+                    sum <= sum + feature_map[i][j]; //sum of 36 product numbers
                 end
-            end
-            feature_map <= sum;
+            end 
+            data_out = sum;
         end
     end 
 
-    assign data_out = feature_map
-
+    assign data_out = feature_map;
 
 endmodule
-
--- src/tb.v
+++ src/tb.v
@@ -7,6 +7,46 @@ that can be driven / tested by the cocotb test.py
 */
 
 // testbench is controlled by test.py
+// module tb ();
+
+//     // this part dumps the trace to a vcd file that can be viewed with GTKWave
+//     initial begin
+//         $dumpfile ("tb.vcd");
+//         $dumpvars (0, tb);
+//         #1;
+//     end
+
+//     // wire up the inputs and outputs
+//     reg  clk;
+//     reg  rst_n;
+//     reg  ena;
+//     reg  [7:0] ui_in;
+//     reg  [7:0] uio_in;
+
+//     wire [6:0] segments = uo_out[6:0];
+//     wire [7:0] uo_out;
+//     wire [7:0] uio_out;
+//     wire [7:0] uio_oe;
+
+
+//     tt_um_suhrojo tt_um_suhrojo(
+//     // include power ports for the Gate Level test
+//     `ifdef GL_TEST
+//         .VPWR( 1'b1),
+//         .VGND( 1'b0),
+//     `endif
+//         .ui_in      (ui_in),    // Dedicated inputs
+//         .uo_out     (uo_out),   // Dedicated outputs
+//         .uio_in     (uio_in),   // IOs: Input path
+//         .uio_out    (uio_out),  // IOs: Output path
+//         .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+//         .ena        (ena),      // enable - goes high when design is selected
+//         .clk        (clk),      // clock
+//         .rst_n      (rst_n)     // not reset
+//         );
+
+// endmodule
+
 module tb ();
 
     // this part dumps the trace to a vcd file that can be viewed with GTKWave
@@ -19,27 +59,20 @@ module tb ();
     // wire up the inputs and outputs
     reg  clk;
     reg  rst_n;
-    reg  ena;
-    reg  [7:0] ui_in;
-    reg  [7:0] uio_in;
+    reg  [5:0][5:0] data_in;
+    reg  [5:0][5:0] kernel;
 
-    wire [6:0] segments = uo_out[6:0];
-    wire [7:0] uo_out;
-    wire [7:0] uio_out;
-    wire [7:0] uio_oe;
+    wire data_out;
 
-    tt_um_suhrojo tt_um_suhrojo (
+    convLayer convLayer(
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
         .VGND( 1'b0),
     `endif
-        .ui_in      (ui_in),    // Dedicated inputs
-        .uo_out     (uo_out),   // Dedicated outputs
-        .uio_in     (uio_in),   // IOs: Input path
-        .uio_out    (uio_out),  // IOs: Output path
-        .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-        .ena        (ena),      // enable - goes high when design is selected
+        .data_in    (data_in),
+        .kernel     (.kernel),
+        .data_out   (.data_out),
         .clk        (clk),      // clock
         .rst_n      (rst_n)     // not reset
         );
