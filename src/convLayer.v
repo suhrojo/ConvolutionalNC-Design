a5c7162bec144730de89d9753727328b057cdc4d
suhrojo <rojel1200@gmail.com>
2023-11-03 23:32:22 -0700

    more typos/ syntax

--- src/convLayer.v
+++ src/convLayer.v
@@ -2,7 +2,7 @@
 
 // module convLayer (
 //     input wire clk, // clk input
-//     input wire rst_n, // rst_n input
+//     input wire rst_n_n, // rst_n_n input
 //     input wire [5:0][5:0] data_in, //6x6 data_in from switches
 
 //     output reg [5:0][5:0] data_out //feature_map with summation output
@@ -27,13 +27,13 @@
 //     //   [1 1 1]
 
 //     // apply convolution: 6x6 matrix * 6x6 kernel of 1s = 6x6 feature map
-//     always @(posedge clk or negedge rst_n) begin
-//         if (!rst_n) begin
-//             data_out <= 12'd0; // data_out rst_n to 0
-//             sum <= 12'd0; // rst_n sum to 0
+//     always @(posedge clk or negedge rst_n_n) begin
+//         if (!rst_n_n) begin
+//             data_out <= 12'd0; // data_out rst_n_n to 0
+//             sum <= 12'd0; // rst_n_n sum to 0
 //         end
 //         else begin
-//             sum <= 12'd0; // rst_n sum for holding all 36
+//             sum <= 12'd0; // rst_n_n sum for holding all 36
 //             for (a = 0; a < 6; a = a + 1) begin
 //                 for (b = 0; b < 6; b = b + 1) begin
 //                     feature_map[i][j] <= data_in[a][b] * kernel[i][j]; //applying kernel filter
@@ -50,9 +50,9 @@
 ///////////////////////////////////////// NEW CODE //////////////////////////////////
 
 module convLayer(
-    input wire [7:0] ui_in,  // 8 buttons, un_in[7] is un_in[6]
+    input wire [7:0] ui_in,  // 8 buttons, ui_in[7] is ui_in[6]
     input wire clk,        // Clock input
-    input wire rst_n,        // Reset input
+    input wire rst_n_n,        // Reset input
     output wire [35:0] data_out  // Output data_out as a 36-bit number
 );
 
@@ -64,8 +64,8 @@ module convLayer(
     reg [2:0] row;   // Current row
     reg loading;     // Loading state
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
+    always @(posedge clk or posedge rst_n) begin
+        if (rst_n) begin
             row <= 0;
             loading <= 0;
             output_register <= 0;
@@ -77,10 +77,10 @@ module convLayer(
                 end
             end
         end else begin
-            if (un_in[6]) begin
+            if (ui_in[6]) begin
                 if (row < 6) begin
                     loading <= 1;
-                    data_register[row] <= un_in[5:0];
+                    data_register[row] <= ui_in[5:0];
                     row <= row + 1;
                 end else begin
                     loading <= 0;
@@ -88,7 +88,7 @@ module convLayer(
                 end
             end
 
-            if (un_in[7]) begin
+            if (ui_in[7]) begin
                 output_register <= 0;
                 sum_register <= 0;
                 for (int i = 0; i < 6; i = i + 1) begin
