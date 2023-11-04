f1230275a4825a41464957c7b42141a3feca9c91
suhrojo <rojel1200@gmail.com>
2023-11-03 23:17:50 -0700

    new approach, all arrays and button login in convLayer module, top is only button connection

--- src/convLayer.v
+++ src/convLayer.v
@@ -1,47 +1,107 @@
 `default_nettype none
 
-module convLayer (
-    input wire clk, // clk input
-    input wire rst_n, // reset input
-    input wire [5:0][5:0] data_in, //6x6 data_in from switches
+// module convLayer (
+//     input wire clk, // clk input
+//     input wire rst_n, // rst_n input
+//     input wire [5:0][5:0] data_in, //6x6 data_in from switches
 
-    output reg [5:0][5:0] data_out //feature_map with summation output
+//     output reg [5:0][5:0] data_out //feature_map with summation output
+// );
+//     reg [5:0][5:0] kernel; //6x6 predefined filter kernel
+//     reg [5:0][5:0] feature_map; //feature_map output before summation of products
+//     reg [11:0] sum; //feature_map sum
+//     integer i, j, a, b;
+
+//     // predefined 6x6 kernel matrix (1s)
+//     initial begin
+//         for (i = 0; i < 6; i = i + 1) begin
+//             for (j = 0; j < 6; j = j + 1) begin
+//                 kernel[i][j] = 6'd1;
+//             end 
+//         end
+//     end
+
+//     // kernel [i][j]
+//     //   [1 1 1]
+//     //   [1 1 1]
+//     //   [1 1 1]
+
+//     // apply convolution: 6x6 matrix * 6x6 kernel of 1s = 6x6 feature map
+//     always @(posedge clk or negedge rst_n) begin
+//         if (!rst_n) begin
+//             data_out <= 12'd0; // data_out rst_n to 0
+//             sum <= 12'd0; // rst_n sum to 0
+//         end
+//         else begin
+//             sum <= 12'd0; // rst_n sum for holding all 36
+//             for (a = 0; a < 6; a = a + 1) begin
+//                 for (b = 0; b < 6; b = b + 1) begin
+//                     feature_map[i][j] <= data_in[a][b] * kernel[i][j]; //applying kernel filter
+//                     sum <= sum + feature_map[i][j]; //sum of 36 product numbers
+//                 end
+//             end
+//             data_out <= sum;
+//         end
+//     end 
+
+// endmodule
+
+
+///////////////////////////////////////// NEW CODE //////////////////////////////////
+
+module convLayer(
+    input wire [7:0] un_in,  // 8 buttons, un_in[7] is un_in[6]
+    input wire clk,        // Clock input
+    input wire rst,        // Reset input
+    output wire [35:0] data_out  // Output data_out as a 36-bit number
 );
-    reg [5:0][5:0] kernel; //6x6 predefined filter kernel
-    reg [5:0][5:0] feature_map; //feature_map output before summation of products
-    reg [11:0] sum; //feature_map sum
-    integer i, j, a, b;
 
-    // predefined 6x6 kernel matrix (1s)
-    initial begin
-        for (i = 0; i < 6; i = i + 1) begin
-            for (j = 0; j < 6; j = j + 1) begin
-                kernel[i][j] = 6'd1;
-            end 
+    reg [5:0] matrix [5:0][5:0];  // 6x6 matrix to hold 1s and 0s
+    reg [35:0] data_register [5:0];  // Register to store the 6x6 matrix data
+    reg [35:0] output_register;     // Register to store the element-wise multiplication data_out
+    reg [35:0] sum_register;        // Register to store the sum of element-wise multiplication
+
+    reg [2:0] row;   // Current row
+    reg loading;     // Loading state
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            row <= 0;
+            loading <= 0;
+            output_register <= 0;
+            sum_register <= 0;
+            for (int i = 0; i < 6; i = i + 1) begin
+                data_register[i] <= 0;
+                for (int j = 0; j < 6; j = j + 1) begin
+                    matrix[i][j] <= 0;
+                end
+            end
+        end else begin
+            if (un_in[6]) begin
+                if (row < 6) begin
+                    loading <= 1;
+                    data_register[row] <= un_in[5:0];
+                    row <= row + 1;
+                end else begin
+                    loading <= 0;
+                    row <= 0;
+                end
+            end
+
+            if (un_in[7]) begin
+                output_register <= 0;
+                sum_register <= 0;
+                for (int i = 0; i < 6; i = i + 1) begin
+                    for (int j = 0; j < 6; j = j + 1) begin
+                        matrix[i][j] = 1;
+                        output_register[i * 6 + j] = data_register[i] & matrix[i][j];
+                        sum_register <= sum_register + output_register[i * 6 + j];
+                    end
+                end
+            end
         end
     end
 
-    // kernel [i][j]
-    //   [1 1 1]
-    //   [1 1 1]
-    //   [1 1 1]
-
-    // apply convolution: 6x6 matrix * 6x6 kernel of 1s = 6x6 feature map
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            data_out <= 12'd0; // data_out reset to 0
-            sum <= 12'd0; // reset sum to 0
-        end
-        else begin
-            sum <= 12'd0; // reset sum for holding all 36
-            for (a = 0; a < 6; a = a + 1) begin
-                for (b = 0; b < 6; b = b + 1) begin
-                    feature_map[i][j] <= data_in[a][b] * kernel[i][j]; //applying kernel filter
-                    sum <= sum + feature_map[i][j]; //sum of 36 product numbers
-                end
-            end
-            data_out <= sum;
-        end
-    end 
+assign data_out = sum_register;
 
 endmodule
--- src/tt_um_suhrojo.v
+++ src/tt_um_suhrojo.v
@@ -14,45 +14,53 @@ module tt_um_suhrojo(
     assign uio_out = 8'b0;
     assign uio_oe = 8'b0;
 
-    reg [5:0] row[5:0]; // array of reg to store each row
-    integer row_index; // keep track of current row index
-    reg data_load, set_row; 
-    reg [5:0] data_out[5:0][5:0]; // stores output from convLayer kernel filter  
-    reg [5:0] data_in[5:0][5:0]; // stores the 6x6 matrix created from the 6 rows of switch inputs
+    // reg [5:0] row[5:0]; // array of reg to store each row
+    // integer row_index; // keep track of current row index
+    // reg data_load, set_row; 
+    // reg [5:0] data_out[5:0][5:0]; // stores output from convLayer kernel filter  
+    // reg [5:0] data_in[5:0][5:0]; // stores the 6x6 matrix created from the 6 rows of switch inputs
 
-    // Logic for storing ui_in[5:0] switch inputs into arrays of rows
-    always @(posedge clk or negedge rst_n) begin 
-        if (!rst_n) begin
-            data_load <= 1'b0;
-            set_row <= 1'b0;
-            row_index <= 0;
-        end
-        else begin
-            data_load <= ui_in[7];
-            if (ui_in[6] && set_row) begin //if set_row is high then we can store a new row
-                row[row_index] <= ui_in[5:0]; // stores current row of switches in row array
-                row_index <= row_index + 1;
-                set_row <= 1'b0;
-            end 
-            else begin
-                set_row <= ui_in[6]; //sets the set_row based on ui_in[6] switch
-            end 
-        end 
-    end 
+    // // Logic for storing ui_in[5:0] switch inputs into arrays of rows
+    // always @(posedge clk or negedge rst_n) begin 
+    //     if (!rst_n) begin
+    //         data_load <= 1'b0;
+    //         set_row <= 1'b0;
+    //         row_index <= 0;
+    //     end
+    //     else begin
+    //         data_load <= ui_in[7];
+    //         if (ui_in[6] && set_row) begin //if set_row is high then we can store a new row
+    //             row[row_index] <= ui_in[5:0]; // stores current row of switches in row array
+    //             row_index <= row_index + 1;
+    //             set_row <= 1'b0;
+    //         end 
+    //         else begin
+    //             set_row <= ui_in[6]; //sets the set_row based on ui_in[6] switch
+    //         end 
+    //     end 
+    // end 
 
-    // Assign the rows to a 6x6 matrix
-    always @(*) begin
-        if (data_load) begin
-            for (row_index = 0; row_index < 6; row_index = row_index + 1) begin
-                data_in[row_index] <= row[row_index];
-            end
-        end
-    end
+    // // Assign the rows to a 6x6 matrix
+    // always @(*) begin
+    //     if (data_load) begin
+    //         for (row_index = 0; row_index < 6; row_index = row_index + 1) begin
+    //             data_in[row_index] <= row[row_index];
+    //         end
+    //     end
+    // end
 
-    convLayer Conv1 (
+    // convLayer Conv1(
+    //     .clk(clk),
+    //     .rst_n(rst_n),
+    //     .data_in(data_in),
+    //     .data_out(data_out)
+    // );
+
+    /////////////////////////////NEW CODE/////////////////////////////////////////////////
+    convLayer Conv1(
         .clk(clk),
         .rst_n(rst_n),
-        .data_in(data_in),
+        .ui_in(un_in),
         .data_out(data_out)
     );
 
